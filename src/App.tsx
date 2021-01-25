7020bdc438de62b9288b1ced3529de49bd221197
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-01-25 19:15:44 -0300

    Alterando o estado

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import store from './store'
 
 import Catalog from './components/catalog';
+import Cart from './components/cart';
 
 function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   );
 }
